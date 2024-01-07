a44f5121a3f3a17cbfd9da382a3411bd6c58de1d
Edson Junior <137929249+JuniorBhering@users.noreply.github.com>
2024-01-07 10:30:47 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,49 +1,51 @@
-const button = document.querySelector(".button-add-task"); // Botão que adiciona a tarefa
+const buttonClear = document.querySelector("#button-Clear");
+const buttonAdd = document.querySelector("#buttonAdd"); // Botão que adiciona a tarefa
 const input = document.querySelector(".input-task"); // Input que a pessoa digita
 const htmlList = document.querySelector(".task-list");
-let ListOfTasks = [];
+let listOfTasks = [];
 
 function addTask() {
   const taskValue = input.value.trim();
   if (taskValue.length !== 0) {
-    ListOfTasks.push(taskValue);
+    listOfTasks.push({ task: taskValue, done: false });
     input.value = "";
-    console.log(ListOfTasks);
     showTask();
   } else {
     window.alert("Please enter a valid input");
   }
 }
 
-function removeAllTasks() {
-  ListOfTasks = [];
-}
-
-function removeItem(index) {
-  ListOfTasks.splice(index, 1);
-  console.log(index);
+function clearTasks() {
+  listOfTasks = [];
   showTask();
 }
 
-function markAsCompleted(index) {}
+function removeItem(index) {
+  listOfTasks.splice(index, 1);
+  showTask();
+}
+
+function markAsCompleted(index) {
+  listOfTasks[index].done = !listOfTasks[index].done;
+  showTask();
+}
 
 function showTask() {
   let addOnList = "";
 
-  ListOfTasks.forEach((task, index) => {
-    addOnList =
-      addOnList +
-      `<li class="task">
-            <!--LI é carda elemto da lista-->
-            <img src="./img/checked.png" alt="" />
-            <p>${task}</p>
-            <img src="./img/cancel.png" alt="" onclick="removeItem(${index})" />
-            </li>`;
+  listOfTasks.forEach((task, index) => {
+    addOnList += `<li class="task ${task.done ? "done" : ""}">
+              <!--LI é cada elemto da lista-->
+              <img src="./img/checked.png" alt="" onclick="markAsCompleted(${index})" />
+              <p>${task.task}</p>
+              <img src="./img/cancel.png" alt="" onclick="removeItem(${index})" />
+              </li>`;
   });
   htmlList.innerHTML = addOnList;
 }
 
-button.addEventListener("click", addTask);
+buttonAdd.addEventListener("click", addTask);
+buttonClear.addEventListener("click", clearTasks);
 input.addEventListener("keyup", function (event) {
   if (event.key === "Enter") {
     addTask();
